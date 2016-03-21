7bb3a575ecc90825d41677676c0d39ae8613f668
roman.kitainik+github@gmail.com <Zxc123456789456123>
2016-03-21 13:43:58 -0700

    End of Stage 8
    Ads can be edited

--- scripts/app.js
+++ scripts/app.js
@@ -1,69 +1,37 @@
-angular
-	.module('ngClassifieds', ['ngMaterial', 'ui.router'])
-	.config(function($mdThemingProvider,$stateProvider){
-		$mdThemingProvider.theme('default')
-			.primaryPalette('indigo')
-			.accentPalette('orange');
-		$stateProvider
-			.state('classifieds', {
-				controller: 'classifiedsCtrl as vm',
-				url: '/classifieds',
-				templateUrl: 'components/classifieds/classifieds.tpl.html'
-			});
-	});
-/*
-	.directive("oogaBooga", function(){
-		return {
-			// camelCase name gets converted to ooga-booga (kebab-case)
-			// Here we return an object known as
-			// the directive definition object
-			template: "<h1>{{ message.title }}</h1><p>{{ message.para }}</p>"
-		}
-	});
-*/
-/*
-<!--
-MVC:
-	Separate concerns (logic, presentation, behaviour)
-	Smaller units
-	Best for working in teams
-	Keep code DRY = don't repeat yourself
-		Controller connects Model and View
-	Angular is MV*, where * = an agnostic "Whatever".
-	
-	
-	
-Digging into controllers	
-	Scope is the glue that connects the controller and the view
+(function() {
 
-Two-way data-binding
+  "use strict";
 
-Dependency injection is not specific to Angular.
-	It's about how functions are created and handled.
-	We inject dependencies by passing them as parameters to the callback in the controller/directive/service
+  angular
+    .module('ngClassifieds', ['ngMaterial', 'ui.router'])
+    .config(function($mdThemingProvider, $stateProvider, $urlRouterProvider) {
 
-Directives
-	Directives are markers in HTML that can come in the form of the element names (<ooga-booga />) or attributes and their values (ng-controller="classifiedsCtrl" or could be special class). Directives describe the behavior of the elements they refer to. Directives are defined with directive definition objects.
-	
-	E.g. built-in
-		<md-toolbar></md-toolbar>
-		<md-button></md-button>
-	or a custom
-		<hello-world></hello-world>
+      $mdThemingProvider.theme('default')
+        .primaryPalette('indigo')
+        .accentPalette('orange');
 
+      $urlRouterProvider.otherwise('/classifieds');
 
-Data API
-	Back end is self-contained
-	Front end is self-contained
-	Two machines: one in the browser, the other on the server
-	RESTful API
-		RESTful = we can dig deeper. Kinda like dot-notation-API.
+      $stateProvider
+        .state('classifieds', {
+          url: '/classifieds',
+          templateUrl: 'components/classifieds/classifieds.tpl.html',
+          controller: 'classifiedsController as vm'
+        })
+        .state('classifieds.new', {
+          url: '/new',
+          templateUrl: 'components/classifieds/new/classifieds.new.tpl.html',
+          controller: 'newClassifiedsController as vm'
+        })
+        .state('classifieds.edit', {
+          url: '/:id/edit',
+          templateUrl: 'components/classifieds/edit/classifieds.edit.tpl.html',
+          controller: 'editClassifiedsController as vm',
+          params: {
+            classified: null
+          }
+        });
+    });
+    
+})();
 
-HTTP Requests and Promises
-	
-Services and Factories
-	Factory needs to return an object
-	
--->
-
-*/
